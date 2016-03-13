7a3420710953c60504ed0fb1e90791a923d9a006
Maulik229 <maulik22ec@gmail.com>
2016-03-13 13:58:35 -0400

    Added files via upload

--- Admin/User.java
+++ Admin/User.java
@@ -9,18 +9,17 @@ import java.util.logging.Logger;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 
-
-public class Admin extends javax.swing.JFrame{
-	 private Connection con=null;
-	  private Statement s=null;
-	private JTextField adminusername= new JTextField();
-	private JTextField adminpassword= new JTextField();
+public class User extends javax.swing.JFrame{
+	private Connection con=null;
+	private Statement s=null;
+	private JTextField userid= new JTextField();
+	private JTextField password= new JTextField();
 	private JButton login= new JButton("LOGIN"); 
-	
-	  public Admin(){
+	public static String userID;
+	  public User(){
 	        init();}
 	    
-	    public Admin(Connection c) {
+	    public User(Connection c) {
 	        init();
 	        try{
 	            Class.forName("com.mysql.jdbc.Driver");
@@ -33,57 +32,55 @@ public class Admin extends javax.swing.JFrame{
 
 private void init() {
 		
-			adminusername.setHorizontalAlignment(JTextField.LEFT);// Set inputs alignments
-			adminpassword.setHorizontalAlignment(JTextField.LEFT);
+			userid.setHorizontalAlignment(JTextField.LEFT);// Set inputs alignments
+			password.setHorizontalAlignment(JTextField.LEFT);
 			
-			JPanel p1=new JPanel(new GridLayout(0,2)); //Creating a panel for GUI
+			JPanel p1=new JPanel(new GridLayout(1,1,0,0)); //Creating a panel for GUI
 			
-			p1.add(new JLabel("ADMIN"));
-			p1.add(adminusername);
+			p1.add(new JLabel("READER ID"));
+			p1.add(userid);
 			p1.add(new JLabel("PASSWORD"));
-			p1.add(adminpassword);
-			p1.setBorder(new TitledBorder("ADMIN LOGIN"));
+			p1.add(password);
+			p1.setBorder(new TitledBorder("READER LOGIN"));
 			add(p1,BorderLayout.NORTH);
 			setSize(400,300);
-			
 			JPanel p2= new JPanel(new FlowLayout(FlowLayout.CENTER));
 			p2.add(login);
 			add(p2,BorderLayout.CENTER);
+			
 			login.addActionListener(new ButtonListener());
 			
-			
 		}
-		private class ButtonListener implements ActionListener{
+private class ButtonListener implements ActionListener{
 			
 			public void actionPerformed(ActionEvent e){
 				if(e.getActionCommand().equals("LOGIN")){
 					
-					String username = adminusername.getText();
-			        String password = adminpassword.getText();
-			        String query = "select password from admin where id ='"+username+"'";
+					userID = userid.getText();
+			        String pwd = password.getText();
+			        String query =   "select password from reader where readerid = '"+userID+"'";
 			      
 			        try {
 			        	ResultSet r = s.executeQuery(query);
-			        	
 			            if(r == null){
 			                System.out.print("off");
 			            }
 			            if(r.next() == false){
 			                JOptionPane op = new JOptionPane();
-			                op.setMessage("The username "+username+" doesn't exist  ");
+			                op.setMessage("The user "+userID+" doesn't exist  ");
 			                op.setMessageType(0);
 			                JDialog dia = op.createDialog(null,"Error");
 			                dia.setTitle("LOGIN ERROR");
 			                dia.setVisible(true);
 			            }
 			            else{
-			            if(r.getString("password").equals(password)){
+			            if(r.getString("password").equals(pwd)){
 			                dispose();
-			               AdminMenu aMenu = new AdminMenu(con);
-			                aMenu.setVisible(true);
+			               UserMenu uMenu = new UserMenu();
+			                uMenu.setVisible(true);;
 			            }else{
 			                JOptionPane op = new JOptionPane();
-			                op.setMessage("The password for "+username+" user is different  ");
+			                op.setMessage("The password for "+userID+" user is different  ");
 			                op.setMessageType(0);
 			                JDialog dia = op.createDialog(null,"Error");
 			                dia.setTitle("LOGIN ERROR");
@@ -95,11 +92,7 @@ private void init() {
 			            Logger.getLogger(Admin.class.getName()).log(Level.SEVERE, null, ex);
 			        }     
 			        
-			        	
-			        }
 			    }
 			}
-		
-					
-} 
-
+		}
+}
